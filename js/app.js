75df3bae5ea14fdccc742d714e25216236498542
Ivan Rodrigues <ivanknow@gmail.com>
2016-02-20 18:54:44 -0300

    angular controllers

--- js/app.js
+++ js/app.js
@@ -2,14 +2,36 @@
  * 
  */
 
-(function(){
-	
-	var app =  angular.module("datablocks",[]);
-	app.controller("ProjectCtrl",function(){
+(function() {
+
+	var app = angular.module("datablocks", []);
+	app.controller("ProjectCtrl", function() {
 		var vm = this;
+
 		vm.title = "Hello Datablocks - Front-end just tomorrow";
-		
+
+		vm.add = function() {
+			
+		}
+
+		vm.getById = function(id) {
+
+		}
+
+		vm.get = function() {
+
+		}
+
 	});
-	
-	
+	app.controller("ClassCtrl", function() {
+		var vm = this;
+		vm.current = {"id":0,"name":"","attrs":[],"methods":[]};
+		
+		vm.add = function(){}
+		vm.remove = function() {
+
+		}
+
+	});
+
 })()
