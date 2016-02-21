6127e519738d6cff3721f95519de9923bf6e0696
Ivan Rodrigues <ivanknow@gmail.com>
2016-02-20 18:41:05 -0600

    service angular

---
+++ js/datablockes-api.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+	angular.module("datablocks").factory('datablocksApi',
+			[ '$http',datablocksApi ]);
+			
+			function datablocksApi($http) {
+			    
+            }
+			
+			
+})();
